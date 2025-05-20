0628b9a30ccb94301248788143cbc73b9f4ac6c4
sakshi-singh-work <sakshi_s@myyahoo.com>
2025-05-21 00:08:38 +0530

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/avani-ecocare/", // ← important!
+  base: "/avani-ecocare/", // 👈 THIS MUST match your repo name
   plugins: [react()],
 });
